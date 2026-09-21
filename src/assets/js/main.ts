e9407434f8548ce9e31af33aebaf89a6d86eafa6
Jarvis Chao <jarvis.pro73045@gmail.com>
2026-09-21 15:28:55 +0800

    refactor: 修改插件 css 引入位置

--- src/assets/js/main.ts
+++ src/assets/js/main.ts
@@ -3,7 +3,6 @@ import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import Lenis from 'lenis';
 import LazyLoad from 'vanilla-lazyload';
-import 'aos/dist/aos.css';
 
 /* ----------------------------------- */
 /* ------ Custom ------ */
